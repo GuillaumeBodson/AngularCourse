64387b5b36aa5eded86696a224d958afa52cd7de
GuillaumeBodson <bodson.guillaume@gmail.com>
2025-09-29 16:01:26 +0200

    Add header

--- src/app/Layout/header/header.component.ts
+++ src/app/Layout/header/header.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
-  selector: 'app-header.component',
-  imports: [],
+  selector: 'app-header',
+  imports: [
+    MatToolbar,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
