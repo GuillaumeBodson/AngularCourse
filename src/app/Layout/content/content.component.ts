06ced934cddec202cc29cfd3192cea15be13b3d0
GuillaumeBodson <bodson.guillaume@gmail.com>
2025-09-29 16:05:02 +0200

    Add content

--- src/app/Layout/content/content.component.ts
+++ src/app/Layout/content/content.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-content.component',
-  imports: [],
+  selector: 'app-content',
+  imports: [
+    // FormationCatalogComponent
+  ],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
