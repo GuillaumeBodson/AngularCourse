fb0ff41c2094b47cc488855e6be92c6946ecf7b7
Guillaume Bodson <bodson.guillaume@gmail.com>
2025-10-13 21:22:24 +0200

    Implement menu toggle functionality; integrate menu service and update header component

--- src/app/Layout/header/header.component.ts
+++ src/app/Layout/header/header.component.ts
@@ -1,14 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, inject, input} from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
+import {Menu} from '../menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMiniFabButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   imports: [
     MatToolbar,
+    MatIconModule,
+    MatMiniFabButton,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+menuService = inject(Menu);
 
+isOpened = input(false);
+
+  toggleSidenav() {
+    this.menuService.toggle();
+  }
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatNavList} from '@angular/material/list';
@@ -6,6 +6,7 @@ import {FooterComponent} from './Layout/footer/footer.component';
 import {HeaderComponent} from './Layout/header/header.component';
 import {ContentComponent} from './Layout/content/content.component';
 import {MenuComponent} from './Layout/menu/menu.component';
+import {Menu} from './Layout/menu';
 
 
 @Component({
@@ -21,4 +22,5 @@ import {MenuComponent} from './Layout/menu/menu.component';
 })
 export class AppComponent {
 
+  menuService = inject(Menu);
 }
